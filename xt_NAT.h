805f801b6d7d7f7779d8c0542e3c01d6770352cc
gonarh <70197461+gonarh@users.noreply.github.com>
2023-08-04 11:31:21 +0600

    Update xt_NAT.h

--- xt_NAT.h
+++ xt_NAT.h
@@ -10,46 +10,57 @@ struct xt_nat_tginfo {
     uint8_t variant;
 };
 
-#define NETFLOW5_RECORDS_MAX 30
+#define NETFLOW9_RECORDS_MAX 30
 
-struct netflow5_record {
-    __be32		s_addr;
-    __be32		d_addr;
-    __be32		nexthop;
-    __be16		i_ifc;
-    __be16		o_ifc;
-    __be32		nr_packets;
-    __be32		nr_octets;
-    __be32		first_ms;
-    __be32		last_ms;
-    __be16		s_port;
-    __be16		d_port;
-    __u8		reserved;
-    __u8		tcp_flags;
-    __u8		protocol;
-    __u8		tos;
-    __be16		s_as;
-    __be16		d_as;
-    __u8		s_mask;
-    __u8		d_mask;
-    __u16		padding;
+struct netflow9_record {
+    __u8	protocol;
+    __be16	s_port;
+    __be32	s_addr;
+    __be16	d_port;
+    __be32	d_addr;
+    __be32	n_addr;
+    __be16	n_port;
+    __u8	event;
 } __attribute__ ((packed));
 
-/* NetFlow v5 packet */
-struct netflow5_pdu {
-    __be16			version;
-    __be16			nr_records;
-    __be32			ts_uptime; /* ms */
-    __be32			ts_usecs;  /* s  */
-    __be32			ts_unsecs; /* ns */
-    __be32			seq;
-    __u8			eng_type;
-    __u8			eng_id;
-    __u16			sampling;
-    struct netflow5_record	flow[NETFLOW5_RECORDS_MAX];
+/* NetFlow v9 template */
+struct netflow9_template {
+    __be16	FlowSetId;
+    __be16	Length;
+    __be16	TemplateId;
+    __be16	FieldsCount;
+    __be16	proto_id;
+    __be16	proto_len;
+    __be16	s_port_id;
+    __be16	s_port_len;
+    __be16	s_addr_id;
+    __be16	s_addr_len;
+    __be16	d_port_id;
+    __be16	d_port_len;
+    __be16	d_addr_id;
+    __be16	d_addr_len;
+    __be16	n_addr_id;
+    __be16	n_addr_len;
+    __be16	n_port_id;
+    __be16	n_port_len;
+    __be16	s_type_id;
+    __be16	s_type_len;
 } __attribute__ ((packed));
 
-#define NETFLOW5_HEADER_SIZE (sizeof(struct netflow5_pdu) - NETFLOW5_RECORDS_MAX * sizeof(struct netflow5_record))
+/* NetFlow v9 packet */
+struct netflow9_pdu {
+    __be16	version;
+    __be16	nr_records;
+    __be32	ts_uptime; /* ms */
+    __be32	ts_usecs;  /* s  */
+    __be32	seq;
+    __be32	srcID;
+    struct netflow9_template template_V9;
+    __be16	FlowSetId;
+    __be16	FlowSetIdSize;
+    struct netflow9_record flow[NETFLOW9_RECORDS_MAX];
+} __attribute__ ((packed));
+
+#define NETFLOW9_HEADER_SIZE (sizeof(struct netflow9_pdu) - NETFLOW9_RECORDS_MAX * sizeof(struct netflow9_record))
 
 #endif /* _LINUX_NETFILTER_XT_NAT_H */
-
